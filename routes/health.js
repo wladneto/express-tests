ccdce71e70dace67ff0c6d04ae6df4dfcf00563f
Wlad <wlad@crudtec.com.br>
2022-02-28 08:59:22 -0300

    Update health

--- routes/health.js
+++ routes/health.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const packageJson = require('../package.json');
 
 /* GET health listing. */
 router.get('/', function(req, res, next) {
   res.status(200).send(
     {
-      message: 'OK',
+      appName: packageJson.name,
+      version: packageJson.version,
+      status: 'up',
       uptime: process.uptime(),
       timestamp: Date.now()
     }
